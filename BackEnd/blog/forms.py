069c9f7fc61c638bec565b5352b2338c791a80b5
JOVANJEET <48331080+JOVANJEET@users.noreply.github.com>
2023-02-04 23:34:08 -0600

    Delete BackEnd directory
    Shifted to Node js

--- BackEnd/blog/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-from .models import BlogPostForm
-
-
-class BlogPostForm(forms.ModelForm):
-    class Meta:
-        model = BlogPostForm
-        fields = ['title', 'body', 'author', 'date']
