1b6447ee2a381138790129ae7d3bb23d7c1ad794
Jovanjeet Singh Toor <jovanjeet16@gmail.com>
2023-02-10 21:21:02 -0600

    Models update for Basic blog, guide and package pages.

--- Backend/model/guide.js
+++ Backend/model/guide.js
@@ -1,25 +1,34 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const guideSchema = new Schema({
-  guideName: {
-    type: String,
-  },
-  guideEmail: {
-    type: String,
-  },
-  guidePassword: {
-    type: String,
-  },
-  guidePhone: {
-    type: String,
-  },
-  packages: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "Package",
+const guideSchema = new Schema(
+  {
+    guideName: {
+      type: String,
     },
-  ],
-});
+    guideEmail: {
+      type: String,
+    },
+    guidePassword: {
+      type: String,
+    },
+    guidePhone: {
+      type: String,
+    },
+    blogs: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Blog",
+      },
+    ],
+    packages: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Package",
+      },
+    ],
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Guide", guideSchema);
