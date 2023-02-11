ac23994961949837e6d52ca549da96c9d7d6a5a1
Jovanjeet Singh Toor <jovanjeet16@gmail.com>
2023-02-10 21:22:22 -0600

    Added urls for Basic blog, guide and package pages.

--- Backend/routes/guide.js
+++ Backend/routes/guide.js
@@ -18,11 +18,15 @@ router.post(
   guideController.postAddPackage
 );
 router.get("/packagelist", isLoggedin, guideController.getPackageList);
-
+router.post("/delete", isLoggedin, guideController.deletePackage);
 //register
 router.get("/register", isAuth, guideController.getRegister);
 router.post("/register", guideController.postRegister);
 router.post("/login", guideController.postLogin);
 router.get("/login", isAuth, guideController.getLogin);
 router.post("/logout", guideController.postVlogout);
+
+router.get("/addblog", isLoggedin, guideController.getAddBlog);
+router.get("/bloglist", isLoggedin, guideController.getBlogList);
+
 module.exports = router;
