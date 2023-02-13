ad25c056339b0332077b0f91727abd024c942182
Jovanjeet Singh Toor <jovanjeet16@gmail.com>
2023-02-12 21:10:04 -0600

    First prototype for Admin, Blog and guide page

--- Backend/routes/guide.js
+++ Backend/routes/guide.js
@@ -19,6 +19,16 @@ router.post(
 );
 router.get("/packagelist", isLoggedin, guideController.getPackageList);
 router.post("/delete", isLoggedin, guideController.deletePackage);
+
+router.get("/profile", isLoggedin, guideController.getProfile);
+router.get("/edit-profile", isLoggedin, guideController.getEditProfile);
+router.post(
+  "/edit-profile",
+  upload.single("gimage"),
+  isLoggedin,
+  guideController.postEditProfile
+);
+
 //register
 router.get("/register", isAuth, guideController.getRegister);
 router.post("/register", guideController.postRegister);
@@ -26,7 +36,9 @@ router.post("/login", guideController.postLogin);
 router.get("/login", isAuth, guideController.getLogin);
 router.post("/logout", guideController.postVlogout);
 
+//blogs
 router.get("/addblog", isLoggedin, guideController.getAddBlog);
 router.get("/bloglist", isLoggedin, guideController.getBlogList);
+router.post("/blog/view", isLoggedin, guideController.viewBlog);
 
 module.exports = router;
