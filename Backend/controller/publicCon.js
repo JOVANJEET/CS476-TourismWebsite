87cd50676386b8ffce470c49ba3a703f338affc7
Jovanjeet Singh Toor <jovanjeet16@gmail.com>
2023-03-20 14:08:19 -0600

    Fixed Blog/Packages Post Error Fixed Booking Error Fixed Tourist Dashboard Added Username Logged As instead of Dashboard

--- Backend/controller/publicCon.js
+++ Backend/controller/publicCon.js
@@ -1,29 +1,35 @@
 const MainPage = require("../model/mainpage");
 const Package = require("../model/package");
 exports.getMainPage = async (req, res, next) => {
-  let logintype = "none";
-  if (req.session.isAdminLoggedIn) {
-    logintype = "admin";
-  } else if (req.session.isLoggedIn) {
-    logintype = "guide";
-  } else if (req.session.isTouristLoggedIn) {
-    logintype = "tourist";
-  }
-  const trendingBlogs = await Package.find({
-    status: "approved",
-  })
-    .sort({ packageViews: -1 })
-    .limit(4)
-    .exec();
-  MainPage.find()
-    .then((mainpage) => {
-      res.render("pages/landingPage", {
-        mainpage: mainpage,
-        packages: trendingBlogs,
-        logintype: logintype,
-      });
+    let logintype = "none";
+    let user = null;
+    if (req.session.isAdminLoggedIn) {
+        logintype = "admin";
+        user = req.admin;
+    } else if (req.session.isLoggedIn) {
+        logintype = "guide";
+        user = req.guide;
+    } else if (req.session.isTouristLoggedIn) {
+        logintype = "tourist";
+        user = req.tourist;
+    }
+    const trendingBlogs = await Package.find({
+        status: "approved",
     })
-    .catch((err) => {
-      console.log(err);
-    });
+        .sort({ packageViews: -1 })
+        .limit(4)
+        .exec();
+    MainPage.find()
+        .then((mainpage) => {
+            res.render("pages/landingPage", {
+                mainpage: mainpage,
+                packages: trendingBlogs,
+                pageTitle: "Travel World | Home",
+                user: user,
+                logintype: logintype,
+            });
+        })
+        .catch((err) => {
+            console.log(err);
+        });
 };
